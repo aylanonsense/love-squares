e423ded36da8397542b8ee505685e820f49f1950
bridgs <bridgs.dev@gmail.com>
2018-11-23 02:49:51 -0500

    Adding boilerplate main.lua

---
+++ main.lua
@@ -0,0 +1,13 @@
+-- local blah = require 'blah'
+-- local bloo = 1
+
+function love.load()
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+  love.graphics.setColor(0.4, 0.4, 1.0, 1.0)
+  love.graphics.circle("fill", 100, 100, 20, 32)
+end
